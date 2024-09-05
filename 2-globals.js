c6c65f17a951464070d0c793a1e1a6533a63a8ba
ary-an00 <aryankhadgee@gmail.com>
2024-09-05 10:17:17 +0545

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,8 @@
+const names= require('./4-names');
+const sayHi= require('./5-utils')
+const data= require('./6-alternative')
+require('./7-mind')
+// console.log(data)
+// sayHi('messi')
+// sayHi(names.cr7)
+// sayHi('jr10')
---
+++ 4-names.js
@@ -0,0 +1,5 @@
+const secret='super secret'
+const john = 'john'
+const peter ='peter'
+const cr7='cr7'
+module.exports={john,peter,cr7}
